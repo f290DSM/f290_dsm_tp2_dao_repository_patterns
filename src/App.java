e310f778245a74442667e9b0fb3d8751f7dfe69e
BZR4 <BZR4@users.noreply.github.com>
2023-10-04 21:31:04 -0300

    Services Parciais

--- src/App.java
+++ src/App.java
@@ -4,12 +4,14 @@ import dao.factories.ConexaoFactory;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ResultSet rst = ConexaoFactory.getConexao()
-                .createStatement()
-                .executeQuery("select version() as versao;");
+        // ResultSet rst = ConexaoFactory.getConexao()
+        //         .createStatement()
+        //         .executeQuery("select version() as versao;");
 
-        if(rst.next()) {
-            System.out.println(rst.getString("versao"));
-        }
+        // if(rst.next()) {
+        //     System.out.println(rst.getString("versao"));
+        // }
+
+        System.out.println(args[0]);
     }
 }
